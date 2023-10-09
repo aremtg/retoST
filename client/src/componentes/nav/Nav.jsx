7346029d8e86dbfdd053354e1febf0a504fcbf5b
aremtg <tgz57031@gmail.com>
2023-10-09 11:37:26 -0500

    ytu6u

---
+++ client/src/componentes/nav/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav>
+        <div>
+            <ul>
+                hi    
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Nav
