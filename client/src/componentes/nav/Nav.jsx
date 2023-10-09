b02f0d1107bf0e29ba8ada90c6f84e36dcea688f
aremtg <tgz57031@gmail.com>
2023-10-09 11:47:25 -0500

    fondo

--- client/src/componentes/nav/Nav.jsx
+++ client/src/componentes/nav/Nav.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './nav.css'
 
 function Nav() {
   return (
     <nav>
         <div>
             <ul>
-                hi    
+                 hi tatiana 
             </ul>
         </div>
     </nav>
