4fd2b57c38ace4fa0dda70190d1167d1fd8be200
SleepedS1 <edwinsantiago3214@gmail.com>
2023-10-10 07:27:59 -0500

    nose

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import subprocess
+import signal
 from colorama import Fore, Style, init
 init()
+state = False
 
 def push():
     commit = input("Ingresa un comentario: ")
@@ -10,55 +12,64 @@ def push():
 def pull():
     os.system("git pull")
 
-def run():
+def start():
     os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__))))
     subprocess.Popen(["node", "server.js"])
     os.chdir("client")
-    subprocess.Popen(["npm", "run", "dev"], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.Popen(["npm", "run", "dev"])
+    print(Fore.GREEN + "Cliente iniciado en: http://localhost:5173\n" + Style.RESET_ALL)
+    print(Fore.GREEN + "Servidor iniciado en: http://localhost:3000" + Style.RESET_ALL)
+
+def stop():
+    os.system('taskkill /f /im node.exe >nul 2>nul')
+    print(Fore.RED + "Cliente detenido.\n" + Style.RESET_ALL)
+    print(Fore.RED + "Servidor detenido." + Style.RESET_ALL)
 
 def code():
     os.system("code .")
 
 def clear():
+    input("Presione ENTER para continuar.")
     os.system("cls" if os.name == "nt" else "clear")
 
-def main_menu():
-    while True:
-        print("--------------")
-        print("[1] Hacer Push")
-        print("[2] Hacer Pull")
-        print("[3] Iniciar Proyecto")
-        print("[4] Abrir entorno vsCode")
-        print("\n[9] Salir")
-        print("--------------")
-        option = input("Selecciona una opción: ")
 
-        if option == "1":
-            print("Ejecutando.\n")
-            push()
-            input("Presione ENTER para continuar.")
+while True:
+    print("--------------")
+    print("[1] Hacer Push")
+    print("[2] Hacer Pull")
+    if state == False:
+        print("[3] Iniciar Servidor " +Fore.RED+ "(Estado: Offline)" + Style.RESET_ALL)
+    else:
+        print("[3] Apagar Servidor " +Fore.GREEN+ "(Estado: Online)" + Style.RESET_ALL)
+    print("[4] Abrir entorno vsCode")
+    print("\n[9] Salir")
+    print("--------------")
+    option = input("Selecciona una opción: ")
+
+    if option == "1":
+        print("Ejecutando.\n")
+        push()
+        clear()
+    elif option == "2":
+        print("Ejecutando.\n")
+        pull()
+        clear()
+    elif option == "3":
+        print("Ejecutando.\n")
+        if state == False:
+            start()
+            state = True
             clear()
-        elif option == "2":
-            print("Ejecutando.\n")
-            pull()
-            input("Presione ENTER para continuar.")
-            clear()
-        elif option == "3":
-            print("Ejecutando.\n")
-            run()
-            print(Fore.GREEN + "Cliente iniciado en: http://localhost:5173\n" + Style.RESET_ALL)
-            print(Fore.GREEN + "Servidor iniciado en: http://localhost:3000" + Style.RESET_ALL)
-            input("Presione ENTER para continuar.")
-            clear()
-        elif option == "4":
-            print("Ejecutando.\n")
-            code()
-            input("Presione ENTER para continuar.")
-            clear()
-        elif option =="9":
-            break
         else:
+            stop()
+            state = False
             clear()
-
-if __name__ == "__main__":
-    main_menu()
+    elif option == "4":
+        print("Ejecutando.\n")
+        code()
+        clear()
+    elif option =="9":
+        break
+    else:
+        print("Ingresa una opción válida.")
+        clear()
