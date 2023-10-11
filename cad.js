ffb00df1f85cb402095bd0a933833cf869a9d58c
SleepedS1 <edwinsantiago3214@gmail.com>
2023-10-11 07:27:59 -0500

    test

---
+++ cad.js
@@ -0,0 +1,4 @@
+// Se importan los módulos necesarios
+const os = require('os');
+const childProcess = require('child_process');
+const readline = require('readline');
---
+++ run_cad.bat
@@ -0,0 +1,3 @@
+@echo off
+node cad.js
+PAUSE
