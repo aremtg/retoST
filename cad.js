42420b20b86a6071977fbbce372faea1d53bfdb4
SleepedS1 <edwinsantiago3214@gmail.com>
2023-10-11 07:31:30 -0500

    XD

--- cad.js
+++
@@ -1,4 +0,0 @@
-// Se importan los módulos necesarios
-const os = require('os');
-const childProcess = require('child_process');
-const readline = require('readline');
--- run_cad.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-node cad.js
-PAUSE
