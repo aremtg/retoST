ed81bf312b9f09fa41dfa044c1d46c9b2cc0d74f
aremtg <tgz57031@gmail.com>
2023-10-23 08:00:39 -0500

    en la mañana:)

--- frontend/src/components/miniComponents/ProductoCard.jsx
+++ frontend/src/components/miniComponents/ProductoCard.jsx
@@ -10,7 +10,12 @@ const ProductoCard = ({ producto }) => {
       <img src={imageUrl} alt={producto.name} />
       <h1>{producto.name}</h1>
       <p>Precio: ${producto.price}</p>
-      <button className={`${estilos.botonAgregar} shadow-md my_button`}>Agregar</button>
+      <button className={`${estilos.botonAgregar} shadow-md my_button`}>
+        Agregar
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+        </svg>
+      </button>
     </div>
   );
 }
--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -3,7 +3,7 @@ import estilos from './nav.module.css'
 import Logo from './Logo'
 import UserProfile from '../miniComponents/UserImgName'
 import { useState } from 'react';
-const Nav = () =>{                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+const Nav = () => {
   const [estadoMenu, setMenuEstado] = useState(false);
 
   const abrirMenu = () => {
@@ -12,8 +12,8 @@ const Nav = () =>{
 
   return (
     <>
-    <div className={`${estilos.contNav} flex items-center shadow-md ${estadoMenu ? estilos.active : ''}`}>
-        <Logo className={estilos.logo}  />
+      <div className={`${estilos.contNav} flex items-center shadow-md ${estadoMenu ? estilos.active : ''}`}>
+        <Logo className={estilos.logo} />
         <nav className={estilos.nav}>
           <ul className={`${estilos.navList} flex gap-2`}>
             <div className={`${estilos.buscador}`}>
@@ -21,26 +21,32 @@ const Nav = () =>{
             </div>
             <li ><Link to="/" className={`${estilos.linkItem} text-center	rounded-full shadow-md`}>Inicio</Link></li>
             <li ><Link to="/profile" className={`${estilos.linkItem} text-center rounded-full shadow-md`}>Perfil</Link></li>
-            <li ><Link to="/carrito" className={`${estilos.linkItem} text-center rounded-full shadow-md`}></Link></li>
+           
           </ul>
         </nav>
+        <Link to="/car" className={`${estilos.contCarrito} text-center shadow-md`}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-7 h-7">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+            </svg>
+          </Link>
         <Link to="/login" className={estilos.link}>
-           <UserProfile 
-              useruser={'Ingresar'}
-              srcProfileImage="src/img/user_icon.jpg"
-              classNameBoxUser={`${estilos.boxUserNav} boxUserNav`}
-              classNameContImg={estilos.contImg}
+          <UserProfile
+            useruser={'Ingresar'}
+            srcProfileImage="src/img/user_icon.jpg"
+            classNameBoxUser={`${estilos.boxUserNav} boxUserNav`}
+            classNameContImg={estilos.contImg}
           />
         </Link>
+         
         <button className={`${estilos.btnMenu} ${estadoMenu ? estilos.open : ''}`} onClick={abrirMenu}>
           <span className={estilos.bar1}></span>
           <span className={estilos.bar2}></span>
           <span className={estilos.bar3}></span>
         </button>
+       
+
       </div>
-      <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
-      <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><!-- ... --></svg>
-    </span>
+
       <br />
     </>
   )
