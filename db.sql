77ca6f4084cb32e973d342c63d9ae8cf4e3f3f33
SleepedS1 <edwinsantiago3214@gmail.com>
2023-11-17 09:05:47 -0500

    xd

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-11-2023 a las 14:33:07
+-- Tiempo de generación: 17-11-2023 a las 15:05:06
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -30,16 +30,16 @@ SET time_zone = "+00:00";
 CREATE TABLE `productos` (
   `id` int(20) NOT NULL,
   `nombre` varchar(100) NOT NULL,
-  `precio` int(20) NOT NULL
+  `precio` int(20) NOT NULL,
+  `imagen` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id`, `nombre`, `precio`) VALUES
-(13, 'Tatiana', 14),
-(14, 'Santiago', 12);
+INSERT INTO `productos` (`id`, `nombre`, `precio`, `imagen`) VALUES
+(49, 'Torre 15-EF2517LA', 2400000, 'torre_15-ef2517la.png');
 
 --
 -- Índices para tablas volcadas
@@ -59,7 +59,7 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
